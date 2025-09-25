278544df4c4e3806a8674c7ce178dcd746fcb41f
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2025-09-25 23:53:42 +0300

    config fix

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// dotenv.config();
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   define: {
-//     global: "window", // <-- polyfill `global` as `window`
-//   },
-//   server: {
-//     proxy: {
-//       "/users": API_BASE_URL,
-//       "/company": API_BASE_URL,
-//       "/job": API_BASE_URL,
-//       "/jobpost": API_BASE_URL,
-//       "/chat": API_BASE_URL,
-//     },
-//   },
-// });
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  define: {
+    global: "window", // <-- polyfill `global` as `window`
+  },
+});
